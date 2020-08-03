381e77d74703387f1a83b90d4234e8f9aec9bd54
haritonch <haritonch@yahoo.com>
2020-08-03 22:43:16 +0300

    Updates

---
+++ precache-manifest.3b8ff56784512231076e53c93cc6fb23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "114042e4bb7b6764f67d30b31e633c78",
+    "url": "/react-game-of-life/index.html"
+  },
+  {
+    "revision": "4eba40977365808700b3",
+    "url": "/react-game-of-life/static/css/main.320bc91d.chunk.css"
+  },
+  {
+    "revision": "20ed1104bd378c21d098",
+    "url": "/react-game-of-life/static/js/2.a204c8c1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-game-of-life/static/js/2.a204c8c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eba40977365808700b3",
+    "url": "/react-game-of-life/static/js/main.a5d908d5.chunk.js"
+  },
+  {
+    "revision": "56d0628c06fd0267653b",
+    "url": "/react-game-of-life/static/js/runtime-main.a4e5bf91.js"
+  }
+]);
